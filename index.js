77393763df53821d27063c84c96333ce721c7861
Yunus Emre Ayan <67464029+Yunus3emre@users.noreply.github.com>
2023-08-21 06:52:01 +0300

    Update index.js
    port env

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ app.use((req, res) => {
 db();
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(process.env.PORT ||PORT, () => {
     console.log("Server listening on port: " + PORT);
 });
